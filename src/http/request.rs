0cc23de6e161d060771169f998a66dd5de29fe48
devj <power3xz@gmail.com>
2020-10-01 20:24:17 +0900

    add utf8error

--- src/http/request.rs
+++ src/http/request.rs
@@ -2,6 +2,8 @@ use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str;
+use std::str::Utf8Error;
 
 pub struct Request {
     path: String,
@@ -13,7 +15,8 @@ impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        unimplemented!();
+        let request = str::from_utf8(buf)?;
+        unimplemented!()
     }
 }
 
@@ -23,6 +26,13 @@ pub enum ParseError {
     InvalidProtocol,
     InvalidMethod,
 }
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
 impl Display for ParseError {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         write!(f, "{}", self.message())
