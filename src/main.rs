5261b563076067a72cc9c554373fc3da3b9daf18
devj <power3xz@gmail.com>
2020-10-02 17:37:08 +0900

    add environment

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 #![allow(dead_code)]
 
 use server::Server;
+use std::env;
+use website_handler::WebsiteHandler;
 
 mod http;
 mod server;
 mod website_handler;
 
 fn main() {
+    let public_path = env::var("PUBLIC_PATH").unwrap();
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(website_handler::WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
