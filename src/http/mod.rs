9069c8ee96bac69813407c8b79226a5daf66fa61
devj <power3xz@gmail.com>
2020-10-02 16:13:59 +0900

    add statuscode

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -3,6 +3,7 @@ pub use query_string::{QueryString, Value as QueryStringValue};
 pub use request::ParseError;
 pub use request::Request;
 pub use response::Response;
+pub use status_code::StatusCode;
 
 pub mod method;
 pub mod query_string;
